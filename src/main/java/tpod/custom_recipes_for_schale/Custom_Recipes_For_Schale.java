fee829eecbadbda8404463931917ede74c5f5bcf
TPOD14077 <qwertyuiop14077@qq.com>
2024-02-04 14:27:03 +0800

    Initial commit

---
+++ src/main/java/tpod/custom_recipes_for_schale/Custom_Recipes_For_Schale.java
@@ -0,0 +1,17 @@
+package tpod.custom_recipes_for_schale;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Custom_Recipes_For_Schale extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
